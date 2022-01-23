8a450d2c48af4b0aed08bcf8ecc107e218e73b71
Birgit Lexa <birgit.lexa@gmail.com>
2022-01-23 22:14:13 +0100

    Layout forecast tiles

---
+++ app/src/main/java/at/nuceria/weatherdemo/util/MarginItemDecoration.kt
@@ -0,0 +1,21 @@
+package at.nuceria.weatherdemo.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MarginItemDecoration(private val spaceSize: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        with(outRect) {
+            if (parent.getChildAdapterPosition(view) == 0) {
+                left = spaceSize
+            }
+            left = spaceSize
+            right = spaceSize
+        }
+    }
+}
