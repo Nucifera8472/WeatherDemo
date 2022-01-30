1829b2037bb624b7b8932e01552f6544c29a1902
Birgit Lexa <birgit.lexa@gmail.com>
2022-01-30 18:17:02 +0100

    Code cleanup

--- app/src/main/java/at/nuceria/weatherdemo/data/local/AppDatabase.kt
+++ app/src/main/java/at/nuceria/weatherdemo/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import at.nuceria.weatherdemo.data.model.CurrentWeatherData
 import at.nuceria.weatherdemo.data.model.DailyWeatherData
 
-@Database(entities = [CurrentWeatherData::class, DailyWeatherData::class], version = 1)
+@Database(entities = [CurrentWeatherData::class, DailyWeatherData::class], version = 2)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun currentWeatherDao(): CurrentWeatherDao
